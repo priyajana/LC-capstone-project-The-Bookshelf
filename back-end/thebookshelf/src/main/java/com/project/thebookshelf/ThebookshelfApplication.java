254dc798ac3c2903cfe063073691063b93ec61fe
Shanmugapriya Janarthanan <priyajanarthanan@gmail.com>
2025-07-14 10:33:59 -0500

    Adding the back-end

---
+++ back-end/thebookshelf/src/main/java/com/project/thebookshelf/ThebookshelfApplication.java
@@ -0,0 +1,13 @@
+package com.project.thebookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThebookshelfApplication.class, args);
+	}
+
+}
---
+++ back-end/thebookshelf/src/test/java/com/project/thebookshelf/ThebookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.thebookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
