90ac5c71258ad05f12c14d63b8b000b0123fa323
Shanmugapriya Janarthanan <priyajanarthanan@gmail.com>
2025-08-04 20:24:01 -0500

    Added read-only prop for the star rating inputs

--- front-end/src/components/shared/Ratings.jsx
+++ front-end/src/components/shared/Ratings.jsx
@@ -15,15 +15,15 @@ export default function Ratings({rating, onRatingChange,isEditing }) {
   return (
     <div className="rating">
    
-    <input id="star-5" type="radio" name="rating" value="5"  checked={rating === 5} onChange={handleRatingChange} disabled={isEditing}/>
+    <input id="star-5" type="radio" name="rating" value="5"  checked={rating === 5} onChange={handleRatingChange} disabled={!isEditing} readOnly={!isEditing}/>
             <label htmlFor="star-5">★</label>
-            <input id="star-4" type="radio" name="rating"  checked={rating === 4} value="4" onChange={handleRatingChange} disabled={isEditing}/>
+            <input id="star-4" type="radio" name="rating"  checked={rating === 4} value="4" onChange={handleRatingChange} disabled={!isEditing} readOnly={!isEditing}/>
             <label htmlFor="star-4">★</label>
-            <input id="star-3" type="radio" name="rating" value="3"  checked={rating === 3} onChange={handleRatingChange} disabled={isEditing}/>
+            <input id="star-3" type="radio" name="rating" value="3"  checked={rating === 3} onChange={handleRatingChange} disabled={!isEditing} readOnly={!isEditing}/>
             <label htmlFor="star-3">★</label>
-            <input id="star-2" type="radio" name="rating" value="2"  checked={rating === 2} onChange={handleRatingChange} disabled={isEditing}/>
+            <input id="star-2" type="radio" name="rating" value="2"  checked={rating === 2} onChange={handleRatingChange} disabled={!isEditing} readOnly={!isEditing}/>
             <label htmlFor="star-2">★</label>
-            <input id="star-1" type="radio" name="rating" value="1"  checked={rating === 1} onChange={handleRatingChange} disabled={isEditing}/>
+            <input id="star-1" type="radio" name="rating" value="1"  checked={rating === 1} onChange={handleRatingChange} disabled={!isEditing} readOnly={!isEditing}/>
             <label htmlFor="star-1">★</label>
     </div>
   );
